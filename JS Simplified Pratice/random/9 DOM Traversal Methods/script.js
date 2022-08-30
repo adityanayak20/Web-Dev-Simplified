82b92729c35c504a88ec7aaee86d52525ce5e6ae
adityanayak20 <20anayak@gmail.com>
2022-08-30 19:36:36 +0530

    14 DOM MANIPULATION & TRAVERSAL METHODS COMPLETED

---
+++ JS Simplified Pratice/random/9 DOM Traversal Methods/script.js
@@ -0,0 +1,54 @@
+// const grandparent = document.getElementById("grandparent-id")
+// const parents = Array.from(document.getElementsByClassName("parent"))
+
+// changeColor(grandparent)
+// parents.forEach(changeColor)
+
+//------------------------------------------------------------------------
+
+
+// const grandparent = document.querySelector('#grandparent-id')
+// const parents = document.querySelectorAll('.parent')
+
+// changeColor(grandparent)
+// parents.forEach(changeColor)
+
+//------------------------------------------------------------------------
+
+
+// const grandparent = document.querySelector('#grandparent-id')
+// const parents = grandparent.children
+// const parentOne = parents[0]
+// const childrenOfParentOne = parentOne.children
+
+// changeColor(childrenOfParentOne[0])
+
+//------------------------------------------------------------------------
+
+// const childOne = document.querySelector('#child-one')
+// const parentOne =  childOne.parentElement
+// const grandParent = parentOne.parentElement
+
+// changeColor(parentOne)
+// changeColor(grandParent)
+
+//------------------------------------------------------------------------
+
+// const childOne = document.querySelector('#child-one')
+// const grandParent = childOne.closest('.grandparent')
+
+// changeColor(grandParent)
+
+//------------------------------------------------------------------------
+
+// const childOne = document.querySelector('#child-one')
+// const childTwo = childOne.nextElementSibling
+
+// changeColor(childTwo)
+// changeColor(childTwo.previousElementSibling)
+
+// function changeColor(element) {
+//   element.style.backgroundColor = "#333"
+// }
+
+//------------------------------------------------------------------------
