3cf81ed5adc7f2b29ef8b543bf45cde55ecc7eb4
adityanayak20 <20anayak@gmail.com>
2022-10-04 08:31:58 +0530

    gen & itr completed folder completed

--- Javascript/Javascript Advanced Pratice/02- Advanced Features/12. Object Getters and Setters/script.js
+++ Javascript/Javascript Advanced Pratice/02- Advanced Features/12. Object Getters and Setters/script.js
@@ -4,8 +4,20 @@ const person = {
     currentYear = new Date().getFullYear()
     this._birthYear = currentYear - this.age
     return this._birthYear
+  },
+  get location() {
+    return this._location
+  },
+  set location(location) {
+    this._location = location
   }
 }
 
 console.log(person.age)
 console.log(person.birthYear)
+
+person.location = "Agra"
+
+console.log(person.location)
+
+console.log(person)
