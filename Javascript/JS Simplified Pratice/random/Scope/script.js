236912c8cebc4071372620212672787580d56076
adityanayak20 <20anayak@gmail.com>
2022-10-04 22:27:48 +0530

    scope folder created in JS simpl

---
+++ Javascript/JS Simplified Pratice/random/Scope/script.js
@@ -0,0 +1,74 @@
+// console.log(`window this: ${this}`)
+
+// {
+//   console.log(`bracket this: ${this}`)
+// }
+
+function outerFunction() {
+  console.log(`outerFunction this: ${this}`)
+    const innerArrow = () => {
+      console.log(`innerArrow this: ${this}`)
+    }
+    function innerFunction() {
+      console.log(`innerFunction this: ${this}`)
+    }
+    innerArrow()
+    innerFunction()
+}
+//outerFunction()
+
+const outerArrow = () => {
+  console.log(`outerArrow this: ${this}`)
+    function innerFunction() {
+      console.log(`innerFunction this: ${this}`)
+    }
+    const innerArrow = () => {
+      console.log(`innerArrow this: ${this}`)
+    }
+    innerFunction()
+    innerArrow()
+}
+//outerArrow()
+
+
+const person1 = {
+  name: "kyle",
+  age: 26,
+  outerFunction() {
+    console.log(`outerFunction this: ${this}`)
+    console.log(`name: ${this.name} age: ${this.age}`)
+    const innerArrow = () => {
+      console.log(`innerArrow this: ${this}`)
+      console.log(`name: ${this.name} age: ${this.age}`)
+    }
+    function innerFunction() {
+      console.log(`innerFunction this: ${this}`)
+      console.log(`name: ${this.name} age: ${this.age}`)
+    }
+    innerArrow()
+    innerFunction()
+  }
+}
+
+//person1?.outerFunction?.()
+
+const person2 = {
+  name: "sally",
+  age: 24,
+  outerArrow: () => {
+    console.log(`outerArrow this: ${this}`)
+    console.log(`name: ${this.name} age: ${this.age}`)
+    function innerFunction() {
+      console.log(`innerFunction this: ${this}`)
+      console.log(`name: ${this.name} age: ${this.age}`)
+    }
+    const innerArrow = () => {
+      console.log(`innerArrow this: ${this}`)
+      console.log(`name: ${this.name} age: ${this.age}`)
+    }
+    innerFunction()
+    innerArrow()
+  }
+}
+
+//person2?.outerArrow?.()
